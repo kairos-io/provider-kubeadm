03cba3a3409f2713a69c6c71a4d7ef02e493af2f
Piyush Kumar <piyush@spectrocloud.com>
2022-10-03 23:10:04 +0530

    replaced go templatest with cli-runtime printer for configs, removed cni

--- main.go
+++ main.go
@@ -5,21 +5,44 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"github.com/c3os-io/c3os/provider-kubeadm/pkg/config"
-	"gopkg.in/yaml.v2"
 	"path/filepath"
-	"text/template"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/cli-runtime/pkg/printers"
+
+	bootstrapapi "k8s.io/cluster-bootstrap/token/api"
+	bootstraputil "k8s.io/cluster-bootstrap/token/util"
+	kubeadmapiv1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta3"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/c3os-io/c3os/sdk/clusterplugin"
 	yip "github.com/mudler/yip/pkg/schema"
 	"github.com/sirupsen/logrus"
+	bootstraptokenv1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/bootstraptoken/v1"
 )
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	_ = kubeadmapiv1.AddToScheme(scheme)
+}
 
 var configurationPath = "/opt/kubeadm"
 
-func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
+func main() {
+	plugin := clusterplugin.ClusterPlugin{
+		Provider: clusterProvider,
+	}
 
-	// TODO(kpiyush17) add cluster.Options handling, expecting the options to match KubeadmConfig spec
+	if err := plugin.Run(); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 
 	var stages []yip.Stage
 
@@ -28,7 +51,16 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 			Enable: []string{"kubelet"},
 			Start:  []string{"containerd"},
 		},
-		Commands: []string{"sudo sysctl --system"},
+		Commands: []string{
+			"sysctl --system",
+			"modprobe overlay",
+			"modprobe br_netfilter",
+		},
+	}
+
+	if !bootstraputil.IsValidBootstrapToken(cluster.ClusterToken) {
+		logrus.Fatalf("the bootstrap token %s is not of the form %s", cluster.ClusterToken, bootstrapapi.BootstrapTokenPattern)
+		return yip.YipConfig{}
 	}
 
 	stages = append(stages, preStage)
@@ -49,21 +81,12 @@ func clusterProvider(cluster clusterplugin.Cluster) yip.YipConfig {
 	return cfg
 }
 
-func main() {
-	plugin := clusterplugin.ClusterPlugin{
-		Provider: clusterProvider,
-	}
-
-	if err := plugin.Run(); err != nil {
-		logrus.Fatal(err)
-	}
-}
-
 func getInitYipStages(cluster clusterplugin.Cluster) []yip.Stage {
 	kubeadmCfg := getInitNodeConfiguration(cluster)
 	return []yip.Stage{
 		{
-			Name: "Install Kubeadm Configuration Files",
+			Name: "Init Kubeadm",
+			If:   "[ ! -f /opt/kubeadm.init ]",
 			Files: []yip.File{
 				{
 					Path:        filepath.Join(configurationPath, "kubeadm.yaml"),
@@ -71,23 +94,8 @@ func getInitYipStages(cluster clusterplugin.Cluster) []yip.Stage {
 					Content:     kubeadmCfg,
 				},
 			},
-		},
-		{
-			Name: "Kubeadm init",
 			Commands: []string{
 				fmt.Sprintf("kubeadm init --config %s --upload-certs", filepath.Join(configurationPath, "kubeadm.yaml")),
-			},
-		},
-		{
-			Name: "Kubeadm export kubeconfig",
-			Commands: []string{
-				fmt.Sprintf("export KUBECONFIG=%s", "/etc/kubernetes/admin.conf"),
-			},
-		},
-		{
-			Name: "Post-Kubeadm apply Calico CNI",
-			Commands: []string{
-				fmt.Sprintf("kubectl apply -f %s -n kube-system", filepath.Join(configurationPath, "calico.yaml")),
 			},
 		},
 	}
@@ -96,14 +104,15 @@ func getInitYipStages(cluster clusterplugin.Cluster) []yip.Stage {
 func getJoinYipStages(cluster clusterplugin.Cluster) []yip.Stage {
 	kubeadmCfg := getJoinNodeConfiguration(cluster)
 
-	joinCmd := fmt.Sprintf("kubeadm join --config %s", filepath.Join(configurationPath, "kubeadm.yaml"))
+	joinCmd := fmt.Sprintf("kubeadm join --config %s --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests", filepath.Join(configurationPath, "kubeadm.yaml"))
 	if cluster.Role == clusterplugin.RoleControlPlane {
 		joinCmd = joinCmd + " --control-plane"
 	}
 
 	return []yip.Stage{
 		{
-			Name: "Install Kubeadm Configuration Files",
+			Name: "Join Kubeadm",
+			If:   "[ ! -f /opt/kubeadm.join ]",
 			Files: []yip.File{
 				{
 					Path:        filepath.Join(configurationPath, "kubeadm.yaml"),
@@ -124,100 +133,57 @@ func getJoinYipStages(cluster clusterplugin.Cluster) []yip.Stage {
 func getInitNodeConfiguration(cluster clusterplugin.Cluster) string {
 	certificateKey := getCertificateKey(cluster.ClusterToken)
 
-	var initCfg bytes.Buffer
-	_ = yaml.NewEncoder(&initCfg).Encode(config.InitConfiguration{
-		TypeMeta: config.TypeMeta{
-			Kind:       "InitConfiguration",
-			APIVersion: "kubeadm.k8s.io/v1beta2",
-		},
-		BootstrapTokens: []config.BootstrapToken{
-			{
-				Token: cluster.ClusterToken,
+	var initCfg kubeadmapiv1.InitConfiguration
+	var clusterCfg kubeadmapiv1.ClusterConfiguration
+
+	_ = yaml.Unmarshal([]byte(cluster.Options), &initCfg)
+	_ = yaml.Unmarshal([]byte(cluster.Options), &clusterCfg)
+
+	substrs := bootstraputil.BootstrapTokenRegexp.FindStringSubmatch(cluster.ClusterToken)
+
+	initCfg.BootstrapTokens = []bootstraptokenv1.BootstrapToken{
+		{
+			Token: &bootstraptokenv1.BootstrapTokenString{
+				ID:     substrs[1],
+				Secret: substrs[2],
 			},
 		},
-		CertificateKey: certificateKey,
-	})
-
-	var clusterCfg bytes.Buffer
-	_ = yaml.NewEncoder(&clusterCfg).Encode(config.ClusterConfiguration{
-		TypeMeta: config.TypeMeta{
-			Kind:       "ClusterConfiguration",
-			APIVersion: "kubeadm.k8s.io/v1beta2",
-		},
-		ControlPlaneEndpoint: cluster.ControlPlaneHost,
-		Networking: config.Networking{
-			PodSubnet: "192.168.0.0/16", // default pod subnet, will be overridden with the value of user option
-		},
-	})
-
-	var kubeletCfg bytes.Buffer
-	_ = yaml.NewEncoder(&kubeletCfg).Encode(config.KubeletConfiguration{
-		TypeMeta: config.TypeMeta{
-			Kind:       "KubeletConfiguration",
-			APIVersion: "kubelet.config.k8s.io/v1beta1",
-		},
-		CgroupDriver: "systemd",
-	})
-
-	initNodeCfg := config.Init{
-		ClusterConfiguration: clusterCfg.String(),
-		InitConfiguration:    initCfg.String(),
-		KubeletConfiguration: kubeletCfg.String(),
 	}
+	initCfg.CertificateKey = certificateKey
 
-	return parseConfigTemplate(string(cluster.Role), config.InitConfigurationTemplate, initNodeCfg)
+	initPrintr := printers.NewTypeSetter(scheme).ToPrinter(&printers.YAMLPrinter{})
+
+	out := bytes.NewBuffer([]byte{})
+
+	_ = initPrintr.PrintObj(&clusterCfg, out)
+	_ = initPrintr.PrintObj(&initCfg, out)
+
+	return out.String()
 }
 
 func getJoinNodeConfiguration(cluster clusterplugin.Cluster) string {
-	certificateKey := getCertificateKey(cluster.ClusterToken)
+	var joinCfg kubeadmapiv1.JoinConfiguration
+	_ = yaml.Unmarshal([]byte(cluster.Options), &joinCfg)
 
-	joinConfiguration := config.JoinConfiguration{
-		TypeMeta: config.TypeMeta{
-			Kind:       "JoinConfiguration",
-			APIVersion: "kubeadm.k8s.io/v1beta2",
-		},
-		Discovery: config.Discovery{
-			BootstrapToken: config.BootstrapTokenDiscovery{
-				Token:                    cluster.ClusterToken,
-				APIServerEndpoint:        cluster.ControlPlaneHost,
-				UnsafeSkipCAVerification: true,
-			},
-		},
-	}
-
-	if cluster.Role == clusterplugin.RoleControlPlane {
-		joinConfiguration.JoinControlPlane = &config.JoinControlPlane{
-			CertificateKey: certificateKey,
+	if joinCfg.Discovery.BootstrapToken == nil {
+		joinCfg.Discovery.BootstrapToken = &kubeadmapiv1.BootstrapTokenDiscovery{
+			Token:                    cluster.ClusterToken,
+			APIServerEndpoint:        cluster.ControlPlaneHost,
+			UnsafeSkipCAVerification: true,
 		}
 	}
 
-	var joinCfg bytes.Buffer
-	_ = yaml.NewEncoder(&joinCfg).Encode(joinConfiguration)
-
-	var kubeletCfg bytes.Buffer
-	_ = yaml.NewEncoder(&kubeletCfg).Encode(config.KubeletConfiguration{
-		TypeMeta: config.TypeMeta{
-			Kind:       "KubeletConfiguration",
-			APIVersion: "kubeadm.k8s.io/v1beta2",
-		},
-		CgroupDriver: "systemd",
-	})
-
-	joinCpNodeCfg := config.Join{
-		JoinConfiguration:    joinCfg.String(),
-		KubeletConfiguration: kubeletCfg.String(),
+	if cluster.Role == clusterplugin.RoleControlPlane {
+		joinCfg.ControlPlane = &kubeadmapiv1.JoinControlPlane{
+			CertificateKey: getCertificateKey(cluster.ClusterToken),
+		}
 	}
 
-	return parseConfigTemplate(string(cluster.Role), config.JoinConfigurationTemplate, joinCpNodeCfg)
-}
+	joinPrinter := printers.NewTypeSetter(scheme).ToPrinter(&printers.YAMLPrinter{})
 
-func parseConfigTemplate(role, tpl string, input interface{}) string {
-	tm, _ := template.New(role).Parse(tpl)
+	out := bytes.NewBuffer([]byte{})
 
-	var out bytes.Buffer
-	if err := tm.Execute(&out, input); err != nil {
-		logrus.Fatal(err)
-	}
+	_ = joinPrinter.PrintObj(&joinCfg, out)
 
 	return out.String()
 }
