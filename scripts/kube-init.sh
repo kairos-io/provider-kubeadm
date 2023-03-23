8646833f5775f3669737810af95276982437f0e1
Piyush Kumar <k17piyush@gmail.com>
2023-03-23 13:07:18 +0530

    run reset if init fails

--- scripts/kube-init.sh
+++ scripts/kube-init.sh
@@ -9,6 +9,9 @@ set -ex
 
 until kubeadm init --config /opt/kubeadm/kubeadm.yaml --upload-certs --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
 do
-  echo "failed to apply kubeadm init, will retry in 10s";
+  echo "failed to apply kubeadm init, applying reset command";
+  kubeadm reset -f
+  iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+  echo "retrying in 10s"
   sleep 10;
 done;
