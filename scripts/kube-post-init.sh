15e2caba87657bfc9ebd38aeeb99fa1bb7719ce5
Piyush Kumar <k17piyush@gmail.com>
2023-05-26 13:15:35 +0530

    export KUBECONFIG env in post-init script
    Signed-off-by: Piyush Kumar <k17piyush@gmail.com>

--- scripts/kube-post-init.sh
+++ scripts/kube-post-init.sh
@@ -7,6 +7,8 @@ exec 19>> /var/log/kube-post-init.log
 export BASH_XTRACEFD="19"
 set -ex
 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
 secret=$(kubectl get secrets kubeadm-certs -n kube-system -o jsonpath="{['metadata']['ownerReferences'][0]['name']}")
 kubectl get secrets -n kube-system "${secret}" -o yaml | kubectl apply set-last-applied --create-annotation=true -f -
 kubectl get secrets -n kube-system "${secret}" -o yaml | sed '/^\( *\)expiration.*/d' | kubectl apply -f -
