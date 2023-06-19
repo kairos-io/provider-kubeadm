bc5b7e6a009a915015465a4ca4f843db954ab3e0
Piyush Kumar <k17piyush@gmail.com>
2023-06-20 00:27:44 +0530

    backup kube-vip on reset ind join if present (#69)
    Signed-off-by: Piyush Kumar <k17piyush@gmail.com>

--- scripts/kube-init.sh
+++ scripts/kube-init.sh
@@ -7,11 +7,33 @@ exec 19>> /var/log/kube-init.log
 export BASH_XTRACEFD="19"
 set -ex
 
+KUBE_VIP_LOC="/etc/kubernetes/manifests/kube-vip.yaml"
+
 until kubeadm init --config /opt/kubeadm/kubeadm.yaml --upload-certs --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
 do
-  echo "failed to apply kubeadm init, applying reset command";
-  kubeadm reset -f
-  iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+  backup_kube_vip_manifest_if_present
+  echo "failed to apply kubeadm init, applying reset";
+  do_kubeadm_reset
   echo "retrying in 10s"
   sleep 10;
+  restore_kube_vip_manifest_after_reset
 done;
+
+do_kubeadm_reset() {
+  kubeadm reset -f
+  iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+  rm -rf /etc/cni/net.d
+}
+
+backup_kube_vip_manifest_if_present() {
+  if [ -f "$KUBE_VIP_LOC" ]; then
+    cp $KUBE_VIP_LOC /opt/kubeadm/kube-vip.yaml
+  fi
+}
+
+restore_kube_vip_manifest_after_reset() {
+  if [ -f "/opt/kubeadm/kube-vip.yaml" ]; then
+      mkdir -p /etc/kubernetes/manifests
+      cp /opt/kubeadm/kube-vip.yaml $KUBE_VIP_LOC
+  fi
+}
--- scripts/kube-join.sh
+++ scripts/kube-join.sh
@@ -7,11 +7,35 @@ exec 19>> /var/log/kube-join.log
 export BASH_XTRACEFD="19"
 set -ex
 
+NODE_ROLE=$1
+KUBE_VIP_LOC="/etc/kubernetes/manifests/kube-vip.yaml"
+
+
 until kubeadm join --config /opt/kubeadm/kubeadm.yaml --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
 do
+  backup_kube_vip_manifest_if_present
   echo "failed to apply kubeadm join, will retry in 10s";
   kubeadm reset -f
   iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
   echo "retrying in 10s"
   sleep 10;
+  restore_kube_vip_manifest_after_reset
 done;
+
+do_kubeadm_reset() {
+  kubeadm reset -f
+  iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+}
+
+backup_kube_vip_manifest_if_present() {
+  if [ -f "$KUBE_VIP_LOC" ] && [ "$NODE_ROLE" != "worker" ]; then
+    cp $KUBE_VIP_LOC /opt/kubeadm/kube-vip.yaml
+  fi
+}
+
+restore_kube_vip_manifest_after_reset() {
+  if [ -f "/opt/kubeadm/kube-vip.yaml" ] && [ "$NODE_ROLE" != "worker" ]; then
+      mkdir -p /etc/kubernetes/manifests
+      cp /opt/kubeadm/kube-vip.yaml $KUBE_VIP_LOC
+  fi
+}
