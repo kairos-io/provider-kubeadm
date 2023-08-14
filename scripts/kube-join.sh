07ca8e22f6d54cbf51908b1253abd7f4f757ccc1
Piyush Kumar <k17piyush@gmail.com>
2023-08-14 10:50:12 +0530

    support for proxy in apiserver (#83)
    Signed-off-by: Piyush Kumar <k17piyush@gmail.com>

--- scripts/kube-join.sh
+++ scripts/kube-join.sh
@@ -8,6 +8,12 @@ export BASH_XTRACEFD="19"
 set -ex
 
 NODE_ROLE=$1
+
+PROXY_CONFIGURED=$2
+proxy_http=$3
+proxy_https=$4
+proxy_no=$5
+
 KUBE_VIP_LOC="/etc/kubernetes/manifests/kube-vip.yaml"
 do_kubeadm_reset() {
   kubeadm reset -f
@@ -27,13 +33,26 @@ restore_kube_vip_manifest_after_reset() {
   fi
 }
 
-until kubeadm join --config /opt/kubeadm/kubeadm.yaml --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
-do
-  backup_kube_vip_manifest_if_present
-  echo "failed to apply kubeadm join, will retry in 10s";
-  kubeadm reset -f
-  iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
-  echo "retrying in 10s"
-  sleep 10;
-  restore_kube_vip_manifest_after_reset
-done;
+if [ "$PROXY_CONFIGURED" = true ]; then
+  until HTTP_PROXY=$proxy_http http_proxy=$proxy_http HTTPS_PROXY=$proxy_https https_proxy=$proxy_https NO_PROXY=$proxy_no no_proxy=$proxy_no kubeadm join --config /opt/kubeadm/kubeadm.yaml --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
+  do
+    backup_kube_vip_manifest_if_present
+    echo "failed to apply kubeadm join, will retry in 10s";
+    kubeadm reset -f
+    iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+    echo "retrying in 10s"
+    sleep 10;
+    restore_kube_vip_manifest_after_reset
+  done;
+else
+  until kubeadm join --config /opt/kubeadm/kubeadm.yaml --ignore-preflight-errors=DirAvailable--etc-kubernetes-manifests -v=5 > /dev/null
+  do
+   backup_kube_vip_manifest_if_present
+   echo "failed to apply kubeadm join, will retry in 10s";
+   kubeadm reset -f
+   iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X && rm -rf /etc/kubernetes/etcd /etc/kubernetes/manifests /etc/kubernetes/pki
+   echo "retrying in 10s"
+   sleep 10;
+   restore_kube_vip_manifest_after_reset
+  done;
+fi
