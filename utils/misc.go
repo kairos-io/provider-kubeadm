5c673ba5851d8d7fe5a889cca92e89e5f3a4eef2
Dr-N00B <arun.sharma@spectrocloud.com>
2026-03-30 23:08:37 +0530

    fix test cases
    Signed-off-by: Dr-N00B <arun.sharma@spectrocloud.com>

--- utils/misc.go
+++ utils/misc.go
@@ -1,11 +1,12 @@
 package utils
 
 import (
+	"fmt"
+
 	"k8s.io/apimachinery/pkg/util/version"
 
 	"github.com/kairos-io/kairos-sdk/clusterplugin"
 	"github.com/kairos-io/kairos/provider-kubeadm/domain"
-	"github.com/sirupsen/logrus"
 )
 
 func GetClusterRootPath(cluster clusterplugin.Cluster) string {
@@ -19,7 +20,7 @@ func GetClusterRootPath(cluster clusterplugin.Cluster) string {
 func IsKubeadmVersionGreaterThan131(kubernetesVersion string) (int, error) {
 	v1, err := version.ParseSemantic(kubernetesVersion)
 	if err != nil {
-		logrus.Fatalf("Failed to parse kubernetes version [%v]. Err: %v", kubernetesVersion, err)
+		return 0, fmt.Errorf("failed to parse kubernetes version %q: %w", kubernetesVersion, err)
 	}
 
 	return v1.Compare("v1.31.0")
